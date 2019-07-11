13085c2348c5666f062494b4c834499d837ff4a6
磊陈 <276241971@qq.com>
2019-07-11 10:43:41 +0800

    test

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by cl on 2019-7-4.
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public void test(){
+        System.out.println("this is a test（new version）!!!😀");
+    }
+}
