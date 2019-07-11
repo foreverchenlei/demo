b8ac78da9642c115a65716081a4749be54954714
磊陈 <276241971@qq.com>
2019-07-11 16:54:01 +0800

    test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -11,6 +11,6 @@ public class TestController {
 
     @GetMapping("/test")
     public void test(){
-        System.out.println("cl-this is a test!!!😀");
+        System.out.println("bwtc-this is a test!!!😀");
     }
 }
